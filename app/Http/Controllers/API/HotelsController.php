a1a6dffe7f8ffe75e406cd78310c773a53e452ea
Ahmed Saad <a7mad.sa3d.2014@gmail.com>
2017-12-26 17:43:40 +0200

    [Mod] refactor Hotels Controller getHotels() method to follow codeclimate hints

--- app/Http/Controllers/API/HotelsController.php
+++ app/Http/Controllers/API/HotelsController.php
@@ -17,103 +17,80 @@ class HotelsController extends Controller
 {
 
 	/**
-	 * Get Hotels
-	 * @param  Request $request [description]
-	 * @return [type]                  [description]
+	 * Get Hotels API
+	 * @param  Request $request Current Request
+	 * @return response         Hotels
 	 */
     public function getHotels(Request $request)
-    {
-    	// $url = 'https://api.myjson.com/bins/tl0bp';
-
-     //    $client = new \GuzzleHttp\Client();
-     //    $response = $client->get($url);
-
-     //    $data = json_decode($response->getBody(), true);
-        
+    {   
         try {
-            $data = [
-                [
-                    "name" => "Media One Hotel",
-                    "price" => 102.2,
-                    "city" => "dubai",
-                    "availability" => [
-                        [
-                            "from" => "10-10-2020",
-                            "to" => "15-10-2020"
-                        ],[
-                            "from" => "25-10-2020",
-                            "to" => "15-11-2020"
-                        ], [
-                            "from" => "10-12-2020",
-                            "to" => "15-12-2020"
-                        ]
-                    ]
-                ], [
-                    "name" => "Rotana Hotel",
-                    "price" => 80.6,
-                    "city" => "cairo",
-                    "availability" => [
-                        [
-                            "from" => "10-10-2011",
-                            "to" => "12-10-2011"
-                        ], [
-                            "from" => "25-10-2011",
-                            "to" => "10-11-2011"
-                        ], [
-                            "from" => "05-12-2020",
-                            "to" => "18-12-2020"
-                        ]
-                    ]
-                ], [
-                    "name" => "Another Hotel",
-                    "price" => 180.6,
-                    "city" => "cairo",
-                    "availability" => [
-                        [
-                            "from" => "10-10-2020",
-                            "to" => "12-10-2020"
-                        ], [
-                            "from" => "25-10-2020",
-                            "to" => "10-11-2020"
-                        ], [
-                            "from" => "05-12-2020",
-                            "to" => "18-12-2020"
-                        ]
-                    ]
-                ]
-            ];
+            $data = $this->getHotelsFromApi();
+        } catch(\Exception $e) {
+            return $this->sendError('Conection Error while calling API');
+        }
 
+        try {
             $hotelsStore = new HotelsStore($data);
 
-            $hotelsStore->addFilter(new NameFilter($request->get('name')))
-                        ->addFilter(new CityFilter($request->get('city')))
-                        ->addFilter(new PriceFilter($request->get('price')))
-                        ->addFilter(new DateFilter($request->get('date')));
-
-            $hotelsStore = $hotelsStore->applyFilters();
-
-            switch ($request->get('order_by')) {
-                case 'price':
-                    $order = new PriceOrder($request->get('order_direction'));
-                    break;
-                case 'name':
-                    $order = new NameOrder($request->get('order_direction'));
-                default:
-                    $order = new NameOrder($request->get('order_direction'));
-            }
+            $hotelsStore = $this->applyFilters($hotelsStore, $request);
+            $order = $this->getOrderByInstance($request);
 
             $output = $hotelsStore->orderBy($order)
                             ->paginate($request->get('per_page'), $request->get('page'), ['path' => \Request::url()])
                             ->appends($request->except('page'));
 
         	// Send response
-        	return $this->sendResponse( [
-        		'hotels' => $output
-        	] );
+        	return $this->sendResponse([ 'hotels' => $output ]);
 
         } catch(\Exception $e) {
             // Send Error
             return $this->sendError($e->getMessage());
         }
+    }
+
+    /**
+     * Call Hotels Api and get hotels
+     * @return array hotels
+     */
+    private function getHotelsFromApi()
+    {
+        $url = 'https://api.myjson.com/bins/tl0bp';
+        $client = new \GuzzleHttp\Client();
+        $response = $client->get($url);
+        return json_decode($response->getBody(), true)['hotels'];
+    }
+
+    /**
+     * Get Order By Instance according to given request
+     * @param  Request $request Current request instance
+     * @return OrderContract           Order Instance
+     */
+    private function getOrderByInstance(Request $request)
+    {
+        switch ($request->get('order_by')) {
+            case 'price':
+                $order = new PriceOrder($request->get('order_direction'));
+                break;
+            case 'name':
+                $order = new NameOrder($request->get('order_direction'));
+        }
+
+        return $order;
+    }
+
+    /**
+     * Apply Filters to given Hotels Store
+     * @param  HotelsStore &$hotelsStore Store
+     * @param  Request     $request      CurrentRequest instance
+     * @return HotelStore                Filtered HotelStore
+     */
+    private function applyFilters(HotelsStore &$hotelsStore, Request $request)
+    {
+        $hotelsStore->addFilter(new NameFilter($request->get('name')))
+                        ->addFilter(new CityFilter($request->get('city')))
+                        ->addFilter(new PriceFilter($request->get('price')))
+                        ->addFilter(new DateFilter($request->get('date')));
+
+        return $hotelsStore = $hotelsStore->applyFilters();
     }
 }
